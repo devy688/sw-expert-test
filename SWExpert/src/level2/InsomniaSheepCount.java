a9d5cdabcc1f34df600e331c3203312386c4dc1e
user <user@DESKTOP-O85GRPP>
2021-12-29 14:25:51 +0900

    InsomniaSheepCount - Using BufferedReader & boolean[]

--- SWExpert/src/level2/InsomniaSheepCount.java
+++ SWExpert/src/level2/InsomniaSheepCount.java
@@ -1,33 +1,66 @@
 package level2;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.Scanner;
 
 public class InsomniaSheepCount { // 1288. 새로운 불면증 치료법
-	public static void main(String[] args) {
+	private static boolean[] isSelected;
 
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+	public static void main(String[] args) throws IOException {
 
-		for (int i = 1; i <= t; i++) {
-			int input = sc.nextInt();
-			int count = 0;
-			ArrayList<String> result = new ArrayList<>();
-			
-			while(result.size() < 10) {
-				count++;
-				String countedNum = Integer.toString(input * count);
-				String[] strArr = countedNum.split("");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-				for (int k = 0; k < strArr.length; k++) {
-					if (!result.contains(strArr[k])) {
-						result.add(strArr[k]);
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			isSelected = new boolean[10];
+			int input = Integer.parseInt(br.readLine());
+			int num = 0;
+
+			while (true) {
+				boolean flag = false;
+				int val = input * ++num;
+				while (val > 0) {
+					isSelected[val % 10] = true;
+					val /= 10;
+				}
+				for (int i = 0; i < isSelected.length; i++) {
+					if (!isSelected[i]) {
+						flag = true;
+						break;
 					}
 				}
+				if (!flag) break;
 			}
-
-			System.out.printf("#%d %d\n", i, input * count);
+			System.out.println("#" + tc + " " + num * input);
 		}
-		sc.close();
+
+		/* 1. Scanner를 이용한 방법 */
+//		Scanner sc = new Scanner(System.in);
+//		int t = sc.nextInt();
+//
+//		for (int i = 1; i <= t; i++) {
+//			int input = sc.nextInt();
+//			int count = 0;
+//			ArrayList<String> result = new ArrayList<>();
+//			
+//			while(result.size() < 10) {
+//				count++;
+//				String countedNum = Integer.toString(input * count);
+//				String[] strArr = countedNum.split("");
+//
+//				for (int k = 0; k < strArr.length; k++) {
+//					if (!result.contains(strArr[k])) {
+//						result.add(strArr[k]);
+//					}
+//				}
+//			}
+//
+//			System.out.printf("#%d %d\n", i, input * count);
+//		}
+//		sc.close();
 	}
 }
